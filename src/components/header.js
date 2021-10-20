77b7841c277d27019313787ccbe0b1dd0ea61020
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-10-19 21:36:46 -0700

    Adjust import setting

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { Menu } from './menu'
+import Menu from './menu'
 
 const Header = ({ siteTitle }) => (
   <header
