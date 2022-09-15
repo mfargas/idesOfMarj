5a372e3041304a5751f40f516e07ba1136d75e92
Marjoree <65003463+mfargas@users.noreply.github.com>
2022-09-14 23:45:53 -0400

    Update styling and layout

--- src/components/artProjects.js
+++ src/components/artProjects.js
@@ -1,14 +1,39 @@
 import * as React from 'react'
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
+import * as styles from '../styles/artgallery.module.css'
 
 const ArtGallery = () => {
     return (
         <>
-            <div className='container'>
-                <Link to='/' >
-                    item 1
-                </Link>
-                
+            <div className={styles.container}>
+                <div className={styles.galleryItem}>
+                    <div className={styles.itemNum}>
+                        001
+                    </div>
+                    <Link to='/' className={styles.itemLink} >
+                        item 1
+                    </Link>
+                    <hr />
+
+                </div>
+                <div className={styles.galleryItem}>
+                    <div className={styles.itemNum}>
+                        002
+                    </div>
+                    <Link to='/' className={styles.itemLink} >
+                        item 1
+                    </Link>
+                    <hr />
+                </div>
+                <div className={styles.galleryItem}>
+                    <div className={styles.itemNum}>
+                        003
+                    </div>
+                    <Link to='/' className={styles.itemLink}>
+                        item 1
+                    </Link>
+                    <hr />
+                </div>
             </div>
         </>
     );
--- src/components/menu.js
+++ src/components/menu.js
@@ -4,14 +4,15 @@ import { Link } from 'gatsby'
 import * as styles from '../styles/menu.module.css'
 
 const Menu = () => {
-    return(
-            <menu>
-                <ul className={styles.navMenu}>
-                <li className={styles.navItem}> <Link className={styles.navLink} to='/art'>Art</Link></li>
-                    <li className={styles.navItem}> <Link className={styles.navLink} to='/'> Blog</Link></li>
-                </ul>
-            </menu>
-        );
+    return (
+        <menu>
+            <ul className={styles.navMenu}>
+                <li className={styles.navItem}> <Link className={styles.navLink} to='/art'> GALLERY </Link></li>
+                <li className={styles.navItem}> <Link className={styles.navLink} to='/'> ABOUT</Link></li>
+                <li className={styles.navItem}> <Link className={styles.navLink} to='/'> RESOURCES </Link></li>
+            </ul>
+        </menu>
+    );
 }
 
 export default Menu
